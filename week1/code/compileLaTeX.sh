8ecabda334c7f1410d9982d0f050e57b40f62018
Zhou <zy3425@ic.ac.uk>
2025-10-02 21:43:12 +0100

    rename scripts to maintain consistency and complete Latex and Git exercises.

---
+++ week1/code/compileLaTeX.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Compile LaTeX document
+
+if [[ $1 == *.tex ]]; then
+    filename=${1%.tex} # Remove file extension
+else
+    filename=$1 # Use the provided name
+fi
+
+pdflatex $filename.tex
+bibtex $filename
+pdflatex $filename.tex
+pdflatex $filename.tex
+evince $filename.pdf &
+
+## Cleanup
+rm *.aux
+rm *.log
+rm *.bbl
+rm *.blg
